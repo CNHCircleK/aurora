2528dbbd4fd53d1bea9ea3a8dc5ec6440e3f1630
Joseph Le <josephle@me.com>
2017-01-07 17:38:03 -0800

    Added intro message and removed extra page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <h3>Hello CNH Circle K-er!</h3>
 
-                <div class="panel-body">
-                    You are logged in!
+                        <p>
+                            Awards season is upon us! As of <strong>November 18, 2016</strong>, all DCON Awards
+                            Applications were
+                            e-mailed to club executive board officers and Lt. Governors. This year, we are transitioning
+                            to an
+                            Online Awards Submissions System with the great help of District Technology Editor Joseph
+                            Le. Awards season can be extremely stressful, but we hope that you have all the tools you
+                            need to start early, be proactive, and put forth all that you want into these
+                            applications.
+                        </p>
+
+                        <p>
+                            <strong>All awards are Kiwanian judged.</strong> The awards applications serve as a
+                            reflection of all of the
+                            feats and obstacles you conquered this term. We hope that you go into awards feeling that
+                            you did all that you possibly could, and that no matter the outcome at District Convention
+                            2017, the biggest award you could have ever received is being able to conquer the Circle K
+                            year with a wonderful home club. Awards provide you with the opportunity to showcase all
+                            that you were able to accomplish. It allows you to reflect on the highest of highs and the
+                            lowest of lows and it reminds you of how much you were able to grow and develop over the
+                            course of the year. Although awards will give you sleepless nights and stress pimples, they
+                            serve as a personal reflection of the growth of your club. Ultimately, we hope that these
+                            applications are things you are able to look back on and be proud of for terms to come.
+                        </p>
+
+                        <p>
+                            As you dive into awards, please don't forget to make great use of MRP/MRS Coordinator Esther
+                            Wang's Master Records Sheet feedback for inputting information into your applications and
+                            Creative Assistant's Paula Le's <a
+                                    href="https://issuu.com/cnhcirclek/docs/dcon_manual_final_draft">DCON Awards
+                                Manual</a> for encouragement from the MR Committee
+                            and useful tips and tricks! Please be sure to contact any member of the committee if you
+                            ever need help and we wish you the best of luck!
+                        </p>
+
+
+                        <p>With Care,<br/>
+                            The 2016-2017 District Member Recognition Committee</p>
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
