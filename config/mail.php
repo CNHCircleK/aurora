5e81e83d7d551fe148b8820bc03f2c2cdb5a932c
Joseph Le <josephle@me.com>
2017-01-15 12:01:15 -0800

    Fixed email configs

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'noreply@cnhcirclek.org', 'name' => 'CNH Circle K'],
 
     /*
     |--------------------------------------------------------------------------
